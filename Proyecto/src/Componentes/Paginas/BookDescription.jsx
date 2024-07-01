9ff96e66c441886a3d58d57e94d32ad01ca91a9b
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2024-06-30 18:45:16 -0600

    Cambios menores

--- Proyecto/src/Componentes/Paginas/BookDescription.jsx
+++ Proyecto/src/Componentes/Paginas/BookDescription.jsx
@@ -8,13 +8,13 @@ export default function BookDescription (){
         <div className="body-page">
             <Header className = "header-bookDescription"/>
             <Description
-                titulo={"TituloX"}
-                priceOriginal={"$5000"}
-                priceDescount={"$100"}
-                descount={"99% OFF"}
-                yearPublication={"2XXX"}
-                numPages={"1,000,000"}
-                isnbBook={"XXXXXXXXXXX"}
+                titulo={"Algorithm"}
+                priceOriginal={"$2484"}
+                priceDescount={"$1400"}
+                descount={"15% OFF"}
+                yearPublication={"2022"}
+                numPages={"1312 páginas"}
+                isnbBook={"09786073238021"}
                 sipnosisBook="Cada año, se invierten incontables horas y se desperdician recursos debido a código mal escrito, lo que disminuye la productividad, genera fallos graves y puede comprometer la estabilidad de una organización o empresa. El experto en software Robert C. Martin, junto con sus colegas de Object Mentor, ofrece técnicas y metodologías ágiles para mejorar el código de manera continua y crearlo de forma adecuada"
             />
 
